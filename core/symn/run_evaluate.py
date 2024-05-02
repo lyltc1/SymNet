7e20fe00647feb20108e9864434918c977ec1c41
lyltc1 <870767645@qq.com>
2024-05-02 14:24:15 +0000

    evaluate for best ckpt and evaluate bop for symnet zebraCode tless

--- core/symn/run_evaluate.py
+++ core/symn/run_evaluate.py
@@ -2,6 +2,7 @@
 usages:
 python core/symn/run_evaluate.py --eval_folder output/SymNet_ycbv_obj15_xxx
 """
+
 import os
 import sys
 
@@ -26,21 +27,23 @@ from core.symn.utils.renderer import ObjCoordRenderer
 from core.symn.utils.obj import load_objs
 
 
-def write_cvs(evaluation_result_path, file_name_prefix, predictions):
+def write_cvs(
+    evaluation_result_path, file_name_prefix, predictions, bop_eval, bop_visualize
+):
     if not os.path.exists(evaluation_result_path):
         os.makedirs(evaluation_result_path)
     for obj_id, predict_list in predictions.items():
-        filename = file_name_prefix + '-test'
-        filename = os.path.join(evaluation_result_path, filename + '.csv')
+        filename = file_name_prefix + "-test"
+        filename = os.path.join(evaluation_result_path, filename + ".csv")
         with open(filename, "w") as f:
             f.write("scene_id,im_id,obj_id,score,R,t,time\n")
             for data in predict_list:
-                scene_id = data['scene_id']
-                img_id = data['im_id']
-                r = data['R']
-                t = data['t']
-                time = data['time']
-                score = data['score']
+                scene_id = data["scene_id"]
+                img_id = data["im_id"]
+                r = data["R"]
+                t = data["t"]
+                time = data["time"]
+                score = data["score"]
                 r11 = r[0][0]
                 r12 = r[0][1]
                 r13 = r[0][2]
@@ -89,56 +92,103 @@ def write_cvs(evaluation_result_path, file_name_prefix, predictions):
                 f.write(",")
                 # time
                 f.write(f"{str(time)}\n")
-        # os.system("python bop_toolkit/scripts/eval_bop19_pose.py " + f"--result_filenames {os.path.abspath(filename)} " + f"--results_path {os.path.abspath(evaluation_result_path)} " +f"--eval_path {os.path.abspath(evaluation_result_path)}")
-        # os.system("python bop_toolkit/scripts/vis_est_poses.py " + f"--result_filenames {os.path.abspath(filename)} " + f"--output_path {os.path.abspath(evaluation_result_path)}")
+        if bop_eval:
+            os.system(
+                "python bop_toolkit/scripts/eval_bop19_pose.py "
+                + f"--result_filenames {os.path.abspath(filename)} "
+                + f"--results_path {os.path.abspath(evaluation_result_path)} "
+                + f"--eval_path {os.path.abspath(evaluation_result_path)}"
+            )
+        if bop_visualize:
+            os.system(
+                "python bop_toolkit/scripts/vis_est_poses.py "
+                + f"--result_filenames {os.path.abspath(filename)} "
+                + f"--output_path {os.path.abspath(evaluation_result_path)}"
+            )
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--eval_folder", metavar="FILE", help="path to eval folder")
     parser.add_argument("--debug", action="store_true")
-    parser.add_argument('--device', default='cuda:0')
+    parser.add_argument("--device", default="cuda:0")
     parser.add_argument("--ckpt", default="all", help="ckpt name to be evaluated")
     parser.add_argument("--visualize", action="store_true")
+    parser.add_argument("--bop_eval", action="store_true", help="eval by Bop metric")
+    parser.add_argument("--bop_visualize", action="store_true")
+    parser.add_argument("--detection", required=False)
+    parser.add_argument("--dir_suffix", default="")
 
     args = parser.parse_args()
     # parse --eval_folder, generate args.config_file
     assert args.eval_folder
     for file in os.listdir(args.eval_folder):
-        if os.path.splitext(file)[1] == '.py':
+        if os.path.splitext(file)[1] == ".py":
             args.config_file = os.path.join(args.eval_folder, file)
 
     cfg = Config.fromfile(args.config_file)
     # parse --ckpt, generate args.ckpt, do not use ckpt_for_last now
     ckpt = list()
     for file in os.listdir(args.eval_folder):
-        if os.path.splitext(file)[1] == '.ckpt':
+        if os.path.splitext(file)[1] == ".ckpt":
             ckpt.append(file)
     if args.ckpt == "all":
         args.ckpt = ckpt
+    elif args.ckpt == "best":
+        args.ckpt = [ck for ck in ckpt if ck !="last.ckpt"]
+        assert len(args.ckpt) == 1, "not able to determine which ckpt to evaluate"
     else:
         assert args.ckpt in ckpt
-        args.ckpt = list(args.ckpt)
+        if isinstance(args.ckpt, str):
+            args.ckpt = [
+                args.ckpt,
+            ]
+
     # parse --debug
     cfg.DEBUG = args.debug
+    # parse --bop
+    if args.bop_visualize is True:
+        assert args.bop_eval is True, "bop_visualize is based on bop_eval"
+
     # parse device
     device = torch.device(args.device)
+    if args.detection is not None:
+        # parse --detection
+        if "gdrnppdet" in args.detection:
+            cfg.DATASETS.TEST_DETECTION_TYPE = "type2"
+            cfg.DATASETS.TEST_DETECTION_PATH = args.detection
+        elif "zebrapose" in args.detection:
+            cfg.DATASETS.TEST_DETECTION_TYPE = "type1"
+            cfg.DATASETS.TEST_DETECTION_PATH = args.detection
+
     # get info used in calculate metric
     obj_ids = cfg.DATASETS.OBJ_IDS
     dataset_name = cfg.DATASETS.NAME
     meta_info = MetaInfo(dataset_name)
-    models_3d = {obj_id: inout.load_ply(meta_info.model_tpath.format(obj_id=obj_id)) for obj_id in obj_ids}
+    models_3d = {
+        obj_id: inout.load_ply(meta_info.model_tpath.format(obj_id=obj_id))
+        for obj_id in obj_ids
+    }
     models_info = inout.load_json(meta_info.models_info_path, keys_to_int=True)
-    diameters = {obj_id: models_info[obj_id]['diameter'] for obj_id in obj_ids}
+    diameters = {obj_id: models_info[obj_id]["diameter"] for obj_id in obj_ids}
     sym_obj_id = cfg.DATASETS.SYM_OBJS_ID
     if sym_obj_id == "bop":
-        sym_obj_id = [k for k, v in models_info.items() if 'symmetries_discrete' in v or 'symmetries_continuous' in v]
+        sym_obj_id = [
+            k
+            for k, v in models_info.items()
+            if "symmetries_discrete" in v or "symmetries_continuous" in v
+        ]
     objs = load_objs(meta_info, obj_ids)
     renderer = ObjCoordRenderer(objs, [k for k in objs.keys()], cfg.DATASETS.RES_CROP)
+    # CODE_BIT is a new parameter that needs a default value of 16, ensuring compatibility with older configuration
+    cfg.MODEL.GEOMETRY_NET.CODE_BIT = cfg.MODEL.GEOMETRY_NET.get("CODE_BIT", 16)
     # set output_dir and resume
     cfg.OUTPUT_ROOT = os.path.abspath(args.eval_folder)
 
     for ck in args.ckpt:
-        cfg.OUTPUT_DIR = os.path.join(cfg.OUTPUT_ROOT, os.path.splitext(ck)[0])
+        cfg.OUTPUT_DIR = os.path.join(
+            cfg.OUTPUT_ROOT, os.path.splitext(ck)[0] + args.dir_suffix
+        )
         if not os.path.exists(cfg.OUTPUT_DIR):
             os.mkdir(cfg.OUTPUT_DIR)
         cvs_path = cfg.OUTPUT_DIR
@@ -147,34 +197,21 @@ def main():
             os.mkdir(cfg.VIS_DIR)
         cfg.RESUME = os.path.join(cfg.OUTPUT_ROOT, ck)
 
-        # TODO, temporary code to ensure code compatibility, should be deleted after
-        # cfg.DATASETS.TEST_SCORE_THR / TRAIN_CROP / TEST_CROP had been set in config
-        test_score_thr = cfg.DATASETS.get("TEST_SCORE_THR", 0.01)
-        cfg.DATASETS.TEST_SCORE_THR = test_score_thr
-        print(f"cfg.DATASETS.TEST_SCORE_THR is {cfg.DATASETS.TEST_SCORE_THR}, make sure it is the desired behavior")
-        train_crop = cfg.DATASETS.get("TRAIN_CROP", [1.2, 1.5])
-        cfg.DATASETS.TRAIN_CROP = train_crop
-        print(f"cfg.DATASETS.TRAIN_CROP is {cfg.DATASETS.TRAIN_CROP}, make sure it is the desired behavior")
-        test_crop = cfg.DATASETS.get("TEST_CROP", [1.3, 1.3])
-        cfg.DATASETS.TEST_CROP = test_crop
-        print(f"cfg.DATASETS.TEST_CROP is {cfg.DATASETS.TEST_CROP}, make sure it is the desired behavior")
-        code_bit = cfg.MODEL.GEOMETRY_NET.get("CODE_BIT", 16)
-        cfg.MODEL.GEOMETRY_NET.CODE_BIT = code_bit
-        print(f"cfg.MODEL.GEOMETRY_NET.CODE_BIT is {cfg.MODEL.GEOMETRY_NET.CODE_BIT}, make sure it is the desired behavior")
         # build model
         assert cfg.MODEL.NAME == "SymNet"
         model = build_model(cfg)
-        model.load_state_dict(torch.load(cfg.RESUME)['state_dict'])
+        model.load_state_dict(torch.load(cfg.RESUME)["state_dict"])
         model.eval().to(device).freeze()
 
         # load data
         data_test = build_BOP_test_dataset(cfg, cfg.DATASETS.TEST, debug=cfg.DEBUG)
-        loader_test = torch.utils.data.DataLoader(data_test,
-                                                batch_size=1,
-                                                num_workers=4,
-                                                pin_memory=True,
-                                                collate_fn=batch_data_test,
-                                                )
+        loader_test = torch.utils.data.DataLoader(
+            data_test,
+            batch_size=1,
+            num_workers=4,
+            pin_memory=True,
+            collate_fn=batch_data_test,
+        )
         predictions = dict()
         time_forward = []
         for idx, batch in enumerate(tqdm(loader_test)):
@@ -189,8 +226,8 @@ def main():
             out_transes = out_dict["trans"].detach().cpu().numpy()  # [b,3]
 
             for i in range(len(out_rots)):
-                scene_id = batch['scene_id'][i]
-                im_id = batch['img_id'][i]
+                scene_id = batch["scene_id"][i]
+                im_id = batch["img_id"][i]
                 score = batch["det_score"][i] if "det_score" in batch.keys() else 1.0
                 time = batch["det_time"][i] if "det_time" in batch.keys() else 1000.0
 
@@ -203,16 +240,30 @@ def main():
 
                 if obj_id not in predictions:
                     predictions[obj_id] = list()
-                result = {"score": score, "R": est_R, "t": est_t, "gt_R": gt_R, "gt_t": gt_t,
-                        "scene_id": scene_id, "im_id": im_id, "time": time}
+                result = {
+                    "score": score,
+                    "R": est_R,
+                    "t": est_t,
+                    "gt_R": gt_R,
+                    "gt_t": gt_t,
+                    "scene_id": scene_id,
+                    "im_id": im_id,
+                    "time": time,
+                }
                 if args.visualize:
                     visualize_v2(batch, cfg.VIS_DIR, out_dict, renderer=renderer)
                 predictions[obj_id].append(result)
         time_forward = np.array(time_forward)
         print("time_forward", np.mean(time_forward))
         for d in predictions[obj_id]:
-            d["time"] = d["time"]+np.mean(time_forward)
-        write_cvs(cvs_path, cfg.MODEL.NAME + '_' + cfg.DATASETS.NAME, predictions)
+            d["time"] = d["time"] + np.mean(time_forward)
+        write_cvs(
+            cvs_path,
+            cfg.MODEL.NAME + "_" + cfg.DATASETS.NAME,
+            predictions,
+            args.bop_eval,
+            args.bop_visualize,
+        )
 
 
 if __name__ == "__main__":
---
+++ scripts/4_tless_ZebraCode_evaluate_bop.py
@@ -0,0 +1,51 @@
+""" Evaluate BOP for SymNet-ZebraCode under folder output/SymNet_pbr_ZebraCode """
+
+import os
+import sys
+import pandas as pd
+
+sys.path.insert(0, os.getcwd())
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(cur_dir, "../"))
+
+folder_path = "output/SymNet_pbr_ZebraCode"
+dir_suffix = "zebracode"
+output_fn = os.path.join(folder_path, "SymNetZebraCodepbr_tless-test.csv")
+RUN_EVALUATE = True
+GENERATE_CSV = True
+EVAL_BOP = True
+
+if RUN_EVALUATE:
+    eval_folders = []
+    for name in os.listdir(folder_path):
+        if name.startswith("SymNet_tless_obj"):
+            eval_folders.append(name)
+    # check the number of folders to be evaluated
+    assert len(eval_folders) == 30, f"number of folder wrong, {len(eval_folders)} != 30"
+
+    for eval_folder in eval_folders:
+        command = f"python core/symn/run_evaluate.py --eval_folder {os.path.join(folder_path, eval_folder)} --ckpt best --detection zebrapose_detections/tless/tless_bop_pbr_only.json --dir_suffix {dir_suffix}"
+        if os.system(command) != 0:
+            print(f"Command failed, {command}")
+            sys.exit(1)
+
+if GENERATE_CSV:
+    # find all the file "SymNet_tless-test.csv" whose parent folder end with suffix 'zebracode'
+    eval_files = []
+
+    def find_files(start_path, filename, dir_suffix):
+        for root, dirs, files in os.walk(start_path):
+            if root.endswith(dir_suffix) and filename in files:
+                eval_files.append(os.path.join(root, filename))
+
+    find_files(folder_path, "SymNet_tless-test.csv", dir_suffix)
+    print(eval_files)
+    assert len(eval_files) == 30
+
+    combined_csv = pd.concat([pd.read_csv(f) for f in eval_files])
+    combined_csv.to_csv(output_fn, index=False, encoding="utf-8-sig")
+
+if EVAL_BOP:
+    command = f"python bop_toolkit/scripts/eval_bop19_pose.py --result_filenames {output_fn} --results_path {os.path.abspath(folder_path)} --eval_path {os.path.abspath(folder_path)}"
+    print(command)
+    os.system(command)
